ea4f5fbe82a2ec632c96f69aeb0a379a281d24f8
Nigel <s2876731@PC029262.staff.ad.griffith.edu.au>
2018-02-28 16:11:21 +1000

    add nltk

--- SemanticSummarization/main.py
+++ SemanticSummarization/main.py
@@ -3,12 +3,15 @@ import codecs
 import re
 import nltk
 from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
 from nltk.stem.porter import PorterStemmer
+from nltk import ngrams
 
 stop = set(stopwords.words('english'))
 result = set()
 f = codecs.open('C:/Users/s2876731/Desktop/in.txt', 'w', "utf-8")
 uri = 'mongodb://bigdata:databig@localhost/?authSource=admin'
+
 
 def search(myDict, lookup):
     for key, value in myDict.items():
@@ -23,14 +26,21 @@ if __name__ == '__main__':
     database = db_connect[database_name]
     collection = database.collection_names(include_system_collections=False)
 
-    for data in database['goldcoast_location'].find({}, {'text': 1, '_id': 0, 'lang': 1}).sort("_id", -1).limit(100):
-        if data['lang'] == 'en':
-            result = data["text"].replace("\n", " ")
-            result = re.sub(r"https\S+", "", result)
-            result = result.lower()
-            print(result)
-            # print([i for i in result.split() if i not in stop])
-            # f.write(result + "\n")
-            tokens = nltk.word_tokenize(result)
-            tagged = nltk.pos_tag(tokens)
-            #print(tagged)
+stopWords = set(stopwords.words('english'))
+
+for data in database['goldcoast_location'].find({}, {'text': 1, '_id': 0, 'lang': 1}).sort("_id", -1).limit(100):
+    if data['lang'] == 'en':
+        result = data["text"].replace("\n", " ")
+        result = re.sub(r"https\S+", "", result)
+        result = result.lower()
+        resultsplit = result.split()
+
+        stemmer = PorterStemmer()
+        tokens = nltk.word_tokenize(result)
+        tknzr = TweetTokenizer()
+        tweettoken = tknzr.tokenize(result)
+        # tagged = nltk.pos_tag(tokens)
+        # print(tagged)
+        threegrams = ngrams(tweettoken, 3)
+        for grams in threegrams:
+            print(grams)
