d14c07cabe67f8903da01629cbcf22d8178bdf05
Nigel <s2876731@PC029262.staff.ad.griffith.edu.au>
2018-05-11 14:31:38 +1000

    Tidy up the main

--- SemanticSummarization/kmeans.py
+++ SemanticSummarization/kmeans.py
@@ -1,19 +1,26 @@
 import codecs
 
-from sklearn.cluster import KMeans
+from gensim import corpora
+from nltk.corpus import stopwords
+from sklearn.cluster import KMeans, DBSCAN
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import adjusted_rand_score
 
+import logging, gensim
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # f = codecs.open('data/tweets.txt', 'r', "utf-8")
 documents = codecs.open('data/tweets.txt', 'r', "utf-8")
+lda_doc = codecs.open('data/tweets.txt', 'r', "utf-8")
 
 vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(documents)
 
 true_k = 10
-model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
-model.fit(X)
+model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1).fit(X)
+
+dbscan = DBSCAN(metric='euclidean').fit(X)
+
 
 print("Top terms per cluster:")
 order_centroids = model.cluster_centers_.argsort()[:, ::-1]
@@ -21,8 +28,7 @@ terms = vectorizer.get_feature_names()
 for i in range(true_k):
     print("Cluster %d:" % i),
     for ind in order_centroids[i, :10]:
-        print(' %s' % terms[ind]),
-    print
+        print(' %s' % terms[ind])
 
 print("\nPrediction")
 
@@ -34,4 +40,16 @@ Y = vectorizer.transform(["My cat."])
 prediction = model.predict(Y)
 print(prediction)
 
+
+stops = set(stopwords.words("english"))
+
+#LDA
+texts = [[word for word in document.lower().split() if word not in stops] for document in lda_doc]
+dictionary = corpora.Dictionary(texts)
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=3, update_every=1, chunksize=10000, passes=10)
+lda.print_topics(1)
+
 documents.close()
+lda_doc.close()
--- SemanticSummarization/main.py
+++ SemanticSummarization/main.py
@@ -31,71 +31,76 @@ from sumy.utils import get_stop_words
 
 stops = set(stopwords.words("english"))
 stemmer = PorterStemmer()
+
+
 # tknzr = TweetTokenizer()
 # tokens = word_tokenize(result)
 
 # f = codecs.open('tweets.txt', 'w', "utf-8")
-uri = 'mongodb://bigdata:databig@localhost/?authSource=admin'
+
+def mongo_connection(url, db_name, collection_name):
+    final_result = set()
+
+    url = pymongo.MongoClient(url)
+    db_name = url[db_name]
+    # collection = database.collection_names(include_system_collections=False)
+    collection_name = collection_name
+
+    for data in db_name[collection_name]. \
+            find({}, {'_id': 0, 'text': 1, 'extended_tweet': 1, 'lang': 1, 'str_id': 1}).limit(1000):
+        if data['lang'] == 'en':
+            if 'extended_tweet' in data:
+                data['text'] = data['extended_tweet']['full_text']
+
+            result = data["text"].replace("\n", " ")
+            result = result.replace("RT", "")
+            result = re.sub(r"https\S+", "", result)
+            result = re.sub('[^A-Za-z0-9]+', ' ', result).lower()
+
+            # print("ID : %s \nOriginal Text: %s" % (data["id_str"], result))
+            # output_tweet = (data["id_str"], result)
+            # print(output_tweet[0])
+            final_result.add(result)
+
+    return final_result
 
 
-def mongo_connection(self, url, db_name):
-    self.url = pymongo.MongoClient(url)
-    self.db_name = db_connect[db_name]
+def pos_tag(text):
+    tweet_token = simpleTokenize(text)
+    # Use the universal tagger
+    tagged_token = pos_tag([w for w in tweet_token if w not in stops], tagset="universal")
+    # print("Tag : %s" % tagged_token)
+    # Only get specific POS (such as N (Noun), A (Adverb), V (Verb))
+    stem_token = [stemmer.stem(tag) for tag in
+                  [item[0] for item in tagged_token if item[1] == 'ADJ' or item[1] == 'VERB']]
+    # print("Specific tag only: %s" % stem_token + "\n")
+
+    three_grams = ngrams(tagged_token, 3)
+    # for grams in three_grams:
+    # print(grams)
+
+
+def sumy_summarizer(text, lang, sentence_count, sum_type):
+    # url = "http://www.zsstritezuct.estranky.cz/clanky/predmety/cteni/jak-naucit-dite-spravne-cist.html"
+    # parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+    # or for plain text files
+    # TODO : Implement categories / paragraph vectoring beforehand.
+    join_result = ".\n".join(text)
+    parser = PlaintextParser.from_string(join_result, Tokenizer(lang))
+    sumy_stemmer = Stemmer(lang)
+
+    summarizer = sum_type(sumy_stemmer)
+    summarizer.stop_words = get_stop_words(lang)
+
+    for sentence in summarizer(parser.document, sentence_count):
+        print(sentence)
 
 
 if __name__ == '__main__':
-    db_connect = pymongo.MongoClient(uri)
-    database_name = 'twitter'
-    database = db_connect[database_name]
-    collection = database.collection_names(include_system_collections=False)
+    tweet = mongo_connection('localhost', 'twitter', 'budget2018')
 
-final_result = set()
-for data in database['games_sample_100000_aggregation'].\
-        find({}, {'_id': 0, 'text': 1, 'extended_tweet': 1, 'lang': 1, 'str_id': 1}).limit(1000):
-    if data['lang'] == 'en':
-        if 'extended_tweet' in data:
-            data['text'] = data['extended_tweet']['full_text']
-
-        result = data["text"].replace("\n", " ")
-        result = result.replace("RT", "")
-        result = re.sub(r"https\S+", "", result)
-        result = re.sub('[^A-Za-z0-9]+', ' ', result).lower()
-
-        # print("ID : %s \nOriginal Text: %s" % (data["id_str"], result))
-        # output_tweet = (data["id_str"], result)
-        # print(output_tweet[0])
-        final_result.add(result)
-
-        tweet_token = simpleTokenize(result)
-        # Use the universal tagger
-        tagged_token = pos_tag([w for w in tweet_token if w not in stops], tagset="universal")
-        # print("Tag : %s" % tagged_token)
-        # Only get specific POS (such as N (Noun), A (Adverb), V (Verb))
-        stem_token = [stemmer.stem(tag) for tag in
-                      [item[0] for item in tagged_token if item[1] == 'ADJ' or item[1] == 'VERB']]
-        # print("Specific tag only: %s" % stem_token + "\n")
-
-        three_grams = ngrams(tagged_token, 3)
-        # for grams in three_grams:
-        # print(grams)
-
-LANGUAGE = "english"
-SENTENCES_COUNT = 10
-
-# url = "http://www.zsstritezuct.estranky.cz/clanky/predmety/cteni/jak-naucit-dite-spravne-cist.html"
-# parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
-# or for plain text files
-# TODO : Implement categories / paragraph vectoring beforehand.
-join_result = ".\n".join(final_result)
-# print(join_result)
-f.write(join_result)
-f.close()
-
-parser = PlaintextParser.from_string(join_result, Tokenizer(LANGUAGE))
-stemmer = Stemmer(LANGUAGE)
-
-summarizer = LexRank(stemmer)
-summarizer.stop_words = get_stop_words(LANGUAGE)
-
-for sentence in summarizer(parser.document, SENTENCES_COUNT):
-    print(sentence)
+    print(sumy_summarizer(tweet, 'english', 10, TextRank))
+    print("\n")
+    print(sumy_summarizer(tweet, 'english', 10, LexRank))
+    print("\n")
+    print(sumy_summarizer(tweet, 'english', 10, LsaRank))
